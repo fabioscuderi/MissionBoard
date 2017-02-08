c190298124b521d484de295beb969e2496aa6dff
fabioscuderi <scuderifab@gmail.com>
2017-02-08 09:21:55 +0000

    wired to do list

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :todos
+  	resources :todos
 	resources :users, only: [:index, :create, :destroy]
 	resources :roles
 	get "/missionboard" => "missionboard#show", as: "missionboard"
