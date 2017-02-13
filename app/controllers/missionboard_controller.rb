8aa03974dd10f9d7f919af32e4b8b67e33208e05
fabioscuderi <scuderifab@gmail.com>
2017-02-13 13:10:48 +0000

    various changes

--- app/controllers/missionboard_controller.rb
+++ app/controllers/missionboard_controller.rb
@@ -1,5 +1,6 @@
 class MissionboardController < ApplicationController
-	def enter
+
+    def enter
 		session[:current_user_id] = params[:user_id]
 		redirect_to missionboard_path
 	end
@@ -9,12 +10,15 @@ class MissionboardController < ApplicationController
     	@users = User.all
     	@roles = Role.all
     	@todos = Todo.all
-        @moods = Mood.all
-        @categories = Category.all
+      @moods = Mood.all
+      @categories = Category.all
     end
 
     def exit
     	session[:current_user_id] = nil
     	redirect_to root_path
   	end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
 end
